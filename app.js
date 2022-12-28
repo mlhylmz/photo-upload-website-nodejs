6a309ae519771e03af63db0d048b0537b6ee6cac
mlhylmz <melihyilmaz2645@gmail.com>
2022-12-28 03:36:16 +0300

    views added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const nodemon = require('nodemon');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
+
+// TEMPLATE ENGINE
+app.set("view engine", "ejs")
 
 // MIDDLEWARE
 app.use(express.static('public'));
