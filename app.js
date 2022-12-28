1a7fbaa5f5c2956b5af31560ce8678df78873227
mlhylmz <melihyilmaz2645@gmail.com>
2022-12-29 02:46:51 +0300

    routes updated

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const nodemon = require('nodemon');
+var methodOverride = require('method-override');
 const path = require('path');
 const ejs = require('ejs');
 const fs = require('fs');
 const mongoose = require('mongoose');
 
 const Photo = require('./models/photo');
+const { title } = require('process');
 
 const app = express();
 
@@ -21,6 +23,11 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(
+    methodOverride('_method', {
+        methods: ['POST', 'GET'],
+    })
+);
 
 // LISTENING
 const port = 3000;
@@ -35,11 +42,34 @@ app.get('/', async (req, res) => {
         photos,
     });
 });
+
+//PHOTOS
 app.get('/photos/:id', async (req, res) => {
     const photo = await Photo.findById(req.params.id);
     res.render('photo', {
         photo,
     });
+});
+
+// EDIT
+app.get('/photos/edit/:id', async (req, res) => {
+    const photo = await Photo.findOne({ _id: req.params.id });
+    console.log('edit geldi');
+    res.render('edit', {
+        photo,
+    });
+});
+
+// DELETE METHOD
+app.delete('/photos/:id', async (req, res) => {
+    console.log(req.params.id);
+    const photo = await Photo.findOne({ _id: req.params.id });
+    let deletePath = __dirname + '/public' + photo.image;
+    fs.unlinkSync(deletePath);
+
+    await Photo.findByIdAndRemove(req.params.id);
+
+    res.redirect('/');
 });
 
 app.get('/about', (req, res) => {
@@ -48,8 +78,9 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
     res.render('add');
 });
+/* //OLD DATABSE FILE UPLOAD
 app.post('/photos', async (req, res) => {
-    //console.log(req.files);
+    console.log(req.files);
     //await Photo.create(req.body);
     //res.redirect('/');
 
@@ -59,16 +90,69 @@ app.post('/photos', async (req, res) => {
     }
 
     let uploadedImage = req.files.image;
+    console.log("req files : " + uploadedImage);
+    
+    
+
     let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+
+    console.log(uploadPath);
 
     uploadedImage.mv(uploadPath, async () => {
         await Photo.create({
             ...req.body,
             image: '/uploads/' + uploadedImage.name,
         });
+        
+        res.redirect('/');
+    });
+    
+    
+});*/
+
+app.put('/photos/:id', async (req, res) => {
+    console.log('put geldi: ' + req.params.id);
+
+    const photo = await Photo.findOne({ _id: req.params.id });
+    console.log('photo: ' + photo);
+    console.log('req body isim: ' + req.body);
+    //photo.title = req.body.title;
+    //photo.description = req.body.description;
+    photo.save();
+
+    res.redirect(`/photos/${req.params.id}`);
+});
+// NEW DATABSE FILE UPLOAD
+app.post('/photos', async (req, res) => {
+    console.log(req.files);
+    //await Photo.create(req.body);
+    //res.redirect('/');
+
+    const uploadDir = 'public/uploads';
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir);
+    }
+
+    let uploadedImage = req.files.image;
+    console.log('req files : ' + uploadedImage);
+
+    let randomName = Math.random().toString(36).slice(2, 10); // Random Name 8
+    randomName = randomName + '.jpg';
+
+    let uploadPath = __dirname + '/public/uploads/' + randomName;
+
+    console.log(uploadPath);
+
+    uploadedImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image: '/uploads/' + randomName,
+        });
+
         res.redirect('/');
     });
 });
+
 app.get('/photos', (req, res) => {
     res.render('photo');
 });
