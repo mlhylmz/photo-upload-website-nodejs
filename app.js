76a5591798f9a7718c239c181279fdcc11434dc8
mlhylmz <melihyilmaz2645@gmail.com>
2022-12-28 03:17:01 +0300

    files added

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-const fn = function () {
-    console.log('succes');
+const express = require('express');
+const nodemon = require('nodemon');
+
+const app = express();
+
+const myLogger = (req, res, next) => {
+    console.log('middleware log');
+    next();
 };
+
+// MIDDLEWARE
+app.use(express.static('public'));
+app.use(myLogger);
+
+
+const port = 3000;
+
+// LISTENING
+app.listen(port, () => {
+    console.log(`Dinleniyor port :${port}`);
+});
+
+app.get('/', (req, res) => {
+    const photo = {
+        id: 1,
+        name: 'Photo Name',
+        description: 'Photo Desc',
+    };
+    res.send(photo);
+});
